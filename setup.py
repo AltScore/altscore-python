a6aa38371bb1e0fb09429d9978ab4a56d8e477b4
Paulo Castro <paulo.castro@altscore.ai>
2024-02-07 22:34:06 -0600

    fix: minor change -> v0.1.54

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = "Python SDK for AltScore"
 
 setup(
     name="altscore",
-    version="0.1.53",
+    version="0.1.54",
     description="Python SDK for AltScore. It provides a simple interface to the AltScore API.",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
@@ -26,7 +26,8 @@ setup(
         "pydantic<=1.10.12",
         "httpx",
         "stringcase",
-        "python-decouple"
+        "python-decouple",
+        "python-dateutil"
     ],
     extras_require={
         "dev": ["pytest>=7.0", "twine>=4.0.2", "pandas", "tabulate"],
