50e79d1049ce67cd7d4dca6d584fb12f14c9ad4f
Paulo Castro <paulo.castro@altscore.ai>
2024-06-04 14:41:14 -0600

    fix: minor change -> v0.1.140

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = "Python SDK for AltScore"
 
 setup(
     name="altscore",
-    version="0.1.139",
+    version="0.1.140",
     description="Python SDK for AltScore. It provides a simple interface to the AltScore API.",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
