33a2a30bba10cdaced687ec9decd52816b664ae9
ailicic <ilicic.axel@gmail.com>
2025-03-06 12:30:37 -0300

    Typing fixes

--- src/altscore/cms/model/calendars.py
+++ src/altscore/cms/model/calendars.py
@@ -1,5 +1,4 @@
-from typing import Optional
-
+from typing import Optional, List
 from pydantic import BaseModel, Field
 
 
@@ -7,12 +6,13 @@ class CalendarHoliday(BaseModel):
     day: int = Field(alias="day")
     month: str = Field(alias="month")
 
+
 class DPACalendarAPIDTO(BaseModel):
     id: str = Field(alias="calendarId")
     name: str = Field(alias="name")
-    description: str = Optional[Field(alias="description", default=None)]
-    exclusionDays: Optional[list[str]] = Field(alias="exclusionDays", default=None)
-    holidays: Optional[list[CalendarHoliday]] = Field(alias="holidays", default=None)
+    description: Optional[str] = Field(alias="description", default=None)
+    exclusionDays: Optional[List[str]] = Field(alias="exclusionDays", default=None)
+    holidays: Optional[List[CalendarHoliday]] = Field(alias="holidays", default=None)
     type: str = Field(alias="type")
     year: int = Field(alias="year")
 
@@ -21,11 +21,12 @@ class DPACalendarAPIDTO(BaseModel):
         allow_population_by_field_name = True
         populate_by_alias = True
 
+
 class CreateCalendarAPIDTO(BaseModel):
     name: str = Field(alias="name")
     description: Optional[str] = Field(alias="description", default=None)
-    exclusionDays: Optional[list[str]] = Field(alias="exclusionDays", default=None)
-    holidays: Optional[list[str]] = Field(alias="holidays", default=None)
+    exclusionDays: Optional[List[str]] = Field(alias="exclusionDays", default=None)
+    holidays: Optional[List[str]] = Field(alias="holidays", default=None)
 
     class Config:
         populate_by_name = True
